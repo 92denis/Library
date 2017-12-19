7a2cc9ecd65310b8ad10c4fb0afb8cf94bd12d9b
Denis Sadowskiy <92denis-s@mail.ru>
2017-12-19 20:38:12 +0300

    created method getBooks

--- src/app/book.service.ts
+++ src/app/book.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Book } from './book';
 
 @Injectable()
 export class BookService {
 
+  private books: Book[] = [{
+    id: 1,
+    name: 'Windstorm',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Gladzor_University%2C_13-14th_century%2C_village_Vernashen%2C_Vayots_Dzor%2C_Armenia%2C_10.jpg/350px-Gladzor_University%2C_13-14th_century%2C_village_Vernashen%2C_Vayots_Dzor%2C_Armenia%2C_10.jpg',
+    genre: 'Драмма',
+  
+  }, {
+    id: 2,
+    name: 'Wind',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Gladzor_University%2C_13-14th_century%2C_village_Vernashen%2C_Vayots_Dzor%2C_Armenia%2C_10.jpg/350px-Gladzor_University%2C_13-14th_century%2C_village_Vernashen%2C_Vayots_Dzor%2C_Armenia%2C_10.jpg',
+    genre: 'Детектив',
+  }];
   constructor() { }
+
+  getBooks(){
+    return this.books;
+  }
 
 }
