c1b561d323e6e4f3665de25ef2fb6d2e82ad3370
Denis Sadowskiy <92denis-s@mail.ru>
2017-12-19 20:39:41 +0300

    added mat-card in books-item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,22 @@ import { BooksListComponent } from './books-list/books-list.component';
 import { BooksItemComponent } from './books-item/books-item.component';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import { BookService } from './book.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     BooksListComponent,
     BooksItemComponent
+    
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule
   ],
-  providers: [],
+  providers: [BookService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/books-item/books-item.component.ts
+++ src/app/books-item/books-item.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Book } from '../book';
 
 @Component({
-  selector: 'app-books-item',
+  selector: 'books-item',
   templateUrl: './books-item.component.html',
   styleUrls: ['./books-item.component.css']
 })
 export class BooksItemComponent implements OnInit {
 
+  @Input() book: Book;
   constructor() { }
 
   ngOnInit() {
