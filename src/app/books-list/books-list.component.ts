6c4ad00d523f779e7e8d2e4f6b6d05cf8ae7fafc
Denis Sadowskiy <92denis-s@mail.ru>
2017-12-19 20:41:14 +0300

    created getBooks method in books-list component

--- src/app/books-list/books-list.component.ts
+++ src/app/books-list/books-list.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../book';
+import { BookService } from '../book.service';
 
 @Component({
-  selector: 'app-books-list',
+  selector: 'books-list',
   templateUrl: './books-list.component.html',
   styleUrls: ['./books-list.component.css']
 })
 export class BooksListComponent implements OnInit {
 
-  constructor() { }
+  books: Book[];
+  constructor( private bookService: BookService) { }
+
+  getBooks(){
+  this.books = this.bookService.getBooks();
+  }
 
   ngOnInit() {
+   this.getBooks();
   }
 
 }
