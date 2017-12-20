67de78f559283379d03068dda8882f0d50d52237
Denis Sadowskiy <92denis-s@mail.ru>
2017-12-20 15:51:35 +0300

    added two property in book class

--- src/app/book.service.ts
+++ src/app/book.service.ts
@@ -9,12 +9,15 @@ export class BookService {
     name: 'Windstorm',
     image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Gladzor_University%2C_13-14th_century%2C_village_Vernashen%2C_Vayots_Dzor%2C_Armenia%2C_10.jpg/350px-Gladzor_University%2C_13-14th_century%2C_village_Vernashen%2C_Vayots_Dzor%2C_Armenia%2C_10.jpg',
     genre: 'Драмма',
-  
+    description: 'sdfasf',
+    links: []  
   }, {
     id: 2,
     name: 'Wind',
     image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Gladzor_University%2C_13-14th_century%2C_village_Vernashen%2C_Vayots_Dzor%2C_Armenia%2C_10.jpg/350px-Gladzor_University%2C_13-14th_century%2C_village_Vernashen%2C_Vayots_Dzor%2C_Armenia%2C_10.jpg',
     genre: 'Детектив',
+    description: 'sdfasf',
+    links: [] 
   }];
   constructor() { }
 
--- src/app/book.ts
+++ src/app/book.ts
@@ -1,8 +1,12 @@
 export class Book {
-    constructor(
-        public id: number,
-        public name: string,
-        public image: string,
-        public genre: string
-    ) {}
+    public id: number;
+    public name: string;
+    public image: string;
+    public genre: string;
+    public description: string;
+    public links: string[];
+
+    constructor() {
+        this.links = [];
+    }
 }
