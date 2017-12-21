2f6865aa81dc0798d75b3cdaa390f0bc914999a8
Denis Sadowskiy <92denis-s@mail.ru>
2017-12-21 03:24:09 +0300

    created method editBook

--- src/app/books-item/books-item.component.ts
+++ src/app/books-item/books-item.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Book } from '../book';
 import { BookService } from '../book.service';
+import { MatDialog } from '@angular/material';
+import { DialogComponent } from '../dialog/dialog.component';
 
 @Component({
   selector: 'books-item',
@@ -10,12 +12,19 @@ import { BookService } from '../book.service';
 export class BooksItemComponent implements OnInit {
 
   @Input() book: Book;
-  constructor(private bookService: BookService) { }
+  constructor(private bookService: BookService,  public dialog: MatDialog) { }
 
   ngOnInit() {
   }
-  
+
   deleteBook() {
     this.bookService.deleteBook(this.book);
+  }
+
+  editBook(): void {
+    this.dialog.open(DialogComponent, {
+      width: '250px',
+      data: { newBook: this.book}
+    });
   }
 }
